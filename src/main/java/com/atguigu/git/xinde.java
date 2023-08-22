f38ce3229331d76e9504c49b494fcf2dcc06441d
yangfuquan <1148644232@qq.com>
2023-08-22 16:33:30 +0800

    xinde

---
+++ src/main/java/com/atguigu/git/xinde.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: xinde
+ * Description:
+ *
+ * @Create 2023/8/22 16:31
+ * @Version 1.0
+ */
+public class xinde {
+    public static void main(String[] args) {
+        System.out.println("xinde");
+    }
+}
