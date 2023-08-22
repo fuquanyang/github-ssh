dadd26eb2b2d40f9537d31edf572255ac8e77efb
yangfuquan <1148644232@qq.com>
2023-08-22 15:40:33 +0800

    测试

---
+++ src/main/java/com/atguigu/git/test.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: test
+ * Description:
+ *
+ * @Create 2023/8/22 15:33
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("qqq");
+        System.out.println("www");
+    }
+}
