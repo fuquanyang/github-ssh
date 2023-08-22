e306b9804effa3c070e34fecbe490a4e3b0d64f5
yangfuquan <1148644232@qq.com>
2023-08-22 15:53:27 +0800

    测试

---
+++ src/main/java/com/atguigu/git/ssh.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: ssh
+ * Description:
+ *
+ * @Create 2023/8/22 15:52
+ * @Version 1.0
+ */
+public class ssh {
+    public static void main(String[] args) {
+        System.out.println("xcxc");
+    }
+}
