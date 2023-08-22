645ffc0a0af9bd55d413432a1e3cb4b3eb2cafbd
yangfuquan <1148644232@qq.com>
2023-08-22 16:51:57 +0800

    1111

---
+++ src/main/java/com/atguigu/git/xxx.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: xxx
+ * Description:
+ *
+ * @Create 2023/8/22 16:51
+ * @Version 1.0
+ */
+public class xxx {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
